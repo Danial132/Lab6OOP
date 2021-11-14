c2ce99c873223172270e52e76473a19d72623d73
6882899 <6882899@stud.nau.edu.ua>
2021-11-14 17:00:28 +0300

    Add project files.

---
+++ Lab6OOP/Program.cs
@@ -0,0 +1,18 @@
+﻿            using System;
+            using System.IO;
+            using System.Linq;
+
+namespace Laba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(e => Convert.ToInt32(e)).ToArray();
+            Console.WriteLine("Данные из файла:");
+            Console.WriteLine(string.Join(",", array));
+            Console.WriteLine($"Среднее арифметическое:{array.Average()}");
+            Console.WriteLine($"Сумма:{array.Sum()}");
+        }
+    }
+}
